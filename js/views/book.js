d7a48907503f4614da55fd645f68b070180ee2c0
Olly <ollydutton@hotmail.com>
2014-06-10 10:26:23 +1000

    fixed a couple bugs

---
+++ js/views/book.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+    
+    tagName:'div',
+    className: 'bookContainer',
+    template:_.template($('#bookTemplate').html()),
+
+    // events: {
+
+    // },
+
+    // initialize: function() {
+    
+    // },
+
+    render:function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this;
+    }
+});
